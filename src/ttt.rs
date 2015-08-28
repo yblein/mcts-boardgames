6e02615bf1b560c61d9a537cbe25d363a33eeea3
Yoann Blein <yoann.blein@free.fr>
2015-08-28 13:58:20 +0200

    Simplify Tic-tac-toe structure

--- src/ttt.rs
+++ src/ttt.rs
@@ -16,29 +16,6 @@ use utils::draw_board;
 
 const WIDTH: usize = 3;
 
-#[derive(Clone)]
-struct Grid([[Option<Token>; WIDTH]; WIDTH]);
-
-impl Grid {
-	fn empty() -> Grid {
-		Grid([[Default::default(); WIDTH]; WIDTH])
-	}
-}
-
-impl std::ops::Index<Coords2D> for Grid {
-	type Output = Option<Token>;
-
-	fn index<'a>(&'a self, c: Coords2D) -> &'a Option<Token> {
-		&self.0[c.y][c.x]
-	}
-}
-
-impl std::ops::IndexMut<Coords2D> for Grid {
-	fn index_mut<'a>(&'a mut self, c: Coords2D) -> &'a mut Option<Token> {
-		&mut self.0[c.y][c.x]
-	}
-}
-
 #[derive(Copy, Clone, PartialEq, Eq)]
 pub struct Token {
 	player: Player,
@@ -53,79 +30,19 @@ impl Display for Token {
 	}
 }
 
-#[derive(Clone)]
-pub struct Board {
-	grid: Grid,
-}
-
-impl Board {
-	pub fn new() -> Board {
-		Board { grid: Grid::empty() }
-	}
-}
-
-impl TwoPlayerBoard<Move> for Board {
-	fn winner(&self) -> Option<Player> {
-		static LIST: [[Coords2D; 3]; 8] = [
-			[Coords2D { x: 0, y: 0 }, Coords2D { x: 1, y: 0 }, Coords2D { x: 2, y: 0 }],
-			[Coords2D { x: 0, y: 1 }, Coords2D { x: 1, y: 1 }, Coords2D { x: 2, y: 1 }],
-			[Coords2D { x: 0, y: 2 }, Coords2D { x: 1, y: 2 }, Coords2D { x: 2, y: 2 }],
-			[Coords2D { x: 0, y: 0 }, Coords2D { x: 0, y: 1 }, Coords2D { x: 0, y: 2 }],
-			[Coords2D { x: 1, y: 0 }, Coords2D { x: 1, y: 1 }, Coords2D { x: 1, y: 2 }],
-			[Coords2D { x: 2, y: 0 }, Coords2D { x: 2, y: 1 }, Coords2D { x: 2, y: 2 }],
-			[Coords2D { x: 0, y: 0 }, Coords2D { x: 1, y: 1 }, Coords2D { x: 2, y: 2 }],
-			[Coords2D { x: 0, y: 2 }, Coords2D { x: 1, y: 1 }, Coords2D { x: 2, y: 0 }],
-		];
-
-		for v in LIST.iter() {
-			if self.grid[v[0]] != None && v[1..].iter().all(|c| self.grid[*c] == self.grid[v[0]]) {
-				return Some(self.grid[v[0]].unwrap().player);
-			}
-		}
-
-		return None;
-	}
-
-	fn play(&mut self, p: Player, m: &Move) {
-		let Move(pos) = *m;
-		self.grid[pos] = Some(Token { player: p });
-	}
-
-	fn possible_moves_in(&self, _p: Player, moves: &mut Vec<Move>) {
-		if self.winner().is_some() {
-			return;
-		}
-
-		for y in 0..self.grid.0.len() {
-			for x in 0..self.grid.0.len() {
-				if self.grid.0[y][x].is_none() {
-					moves.push(Move(Coords2D { x: x, y: y }))
-				}
-			}
-		}
-	}
-}
-
-impl Display for Board {
-	fn fmt(&self, f: &mut Formatter) -> Result {
-		let b: Vec<&[Option<Token>]> = self.grid.0.iter().map(|r| &r[..]).collect();
-		draw_board(f, &b[..])
-	}
-}
-
 #[derive(Clone, Default)]
 pub struct Move(Coords2D);
 
 impl Debug for Move {
 	fn fmt(&self, f: &mut Formatter) -> Result {
-		write!(f, "{}", self)
+		let Move(pos) = *self;
+		write!(f, "{}", pos)
 	}
 }
 
 impl Display for Move {
 	fn fmt(&self, f: &mut Formatter) -> Result {
-		let Move(pos) = *self;
-		write!(f, "{}", pos)
+		write!(f, "{:?}", self)
 	}
 }
 
@@ -144,5 +61,77 @@ impl Input for Move {
 
 			println!("Impossible move");
 		}
+	}
+}
+
+#[derive(Clone)]
+pub struct Board([[Option<Token>; WIDTH]; WIDTH]);
+
+impl Board {
+	pub fn new() -> Board {
+		Board([[None; WIDTH]; WIDTH])
+	}
+}
+
+impl std::ops::Index<Coords2D> for Board {
+	type Output = Option<Token>;
+
+	fn index<'a>(&'a self, c: Coords2D) -> &'a Option<Token> {
+		&self.0[c.y][c.x]
+	}
+}
+
+impl std::ops::IndexMut<Coords2D> for Board {
+	fn index_mut<'a>(&'a mut self, c: Coords2D) -> &'a mut Option<Token> {
+		&mut self.0[c.y][c.x]
+	}
+}
+
+impl TwoPlayerBoard<Move> for Board {
+	fn winner(&self) -> Option<Player> {
+		static LIST: [[Coords2D; 3]; 8] = [
+			[Coords2D { x: 0, y: 0 }, Coords2D { x: 1, y: 0 }, Coords2D { x: 2, y: 0 }],
+			[Coords2D { x: 0, y: 1 }, Coords2D { x: 1, y: 1 }, Coords2D { x: 2, y: 1 }],
+			[Coords2D { x: 0, y: 2 }, Coords2D { x: 1, y: 2 }, Coords2D { x: 2, y: 2 }],
+			[Coords2D { x: 0, y: 0 }, Coords2D { x: 0, y: 1 }, Coords2D { x: 0, y: 2 }],
+			[Coords2D { x: 1, y: 0 }, Coords2D { x: 1, y: 1 }, Coords2D { x: 1, y: 2 }],
+			[Coords2D { x: 2, y: 0 }, Coords2D { x: 2, y: 1 }, Coords2D { x: 2, y: 2 }],
+			[Coords2D { x: 0, y: 0 }, Coords2D { x: 1, y: 1 }, Coords2D { x: 2, y: 2 }],
+			[Coords2D { x: 0, y: 2 }, Coords2D { x: 1, y: 1 }, Coords2D { x: 2, y: 0 }],
+		];
+
+		for v in LIST.iter() {
+			if self[v[0]] != None && v[1..].iter().all(|c| self[*c] == self[v[0]]) {
+				return Some(self[v[0]].unwrap().player);
+			}
+		}
+
+		return None;
+	}
+
+	fn play(&mut self, p: Player, m: &Move) {
+		let Move(pos) = *m;
+		self[pos] = Some(Token { player: p });
+	}
+
+	fn possible_moves_in(&self, _p: Player, moves: &mut Vec<Move>) {
+		if self.winner().is_some() {
+			return;
+		}
+
+		for y in 0..self.0.len() {
+			for x in 0..self.0.len() {
+				if self.0[y][x].is_none() {
+					moves.push(Move(Coords2D { x: x, y: y }))
+				}
+			}
+		}
+	}
+}
+
+impl Display for Board {
+	fn fmt(&self, f: &mut Formatter) -> Result {
+		let b: Vec<&[Option<Token>]> = self.0.iter().map(|r| &r[..]).collect();
+		draw_board(f, &b[..])
 	}
 }
